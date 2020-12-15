38154ae3c8654d6ca85f765b70ab0ba243df0466
Lisamontin <42090473+Lisamontin@users.noreply.github.com>
2020-12-15 11:29:40 +0100

    add grid constructor

--- models/Grid.cs
+++ models/Grid.cs
@@ -4,4 +4,12 @@ public class Grid {
   public string Description {get; set;}
   public string Name {get; set;}
   public List<Thing> ThingsInGrid {get; set;}
+
+  public Grid(string name, string desc) 
+  {
+    Name = name;
+    Description = desc;
+
+    ThingsInGrid = new List<Thing>();
+  }
 }
