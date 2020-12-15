94fb74f84dfab5b6ae8a6697ee85ad454aca02d1
Lisamontin <42090473+Lisamontin@users.noreply.github.com>
2020-12-15 14:33:28 +0100

    add hero at coordinates, add prompts

--- Program.cs
+++ Program.cs
@@ -8,6 +8,20 @@ namespace Gosen_adventures
         {
             World w = new World();
             w.Init(4);
+
+            Character theHero = new Character("SuperLink", 100, 10);
+
+            Console.WriteLine(World.Map[theHero.Coordinates[0], theHero.Coordinates[3]].Description);
+
+            string command = string.Empty;
+            while (command != "avsluta")
+            {
+                Console.WriteLine(">");
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("Hej då!");
+            Console.ReadLine();
         }
     }
 }
