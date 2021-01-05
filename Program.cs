c9871fb389bcf29be1b8591028244b09187741d6
Lisamontin <42090473+Lisamontin@users.noreply.github.com>
2021-01-05 15:36:06 +0100

    add planning comments

--- Program.cs
+++ Program.cs
@@ -47,3 +47,15 @@ namespace Gosen_adventures
 
     }
 }
+
+
+// planning //
+
+// Finish Game //
+// if theHero enters coordinates .. && has the key in the inventory arr, the "chest" is opened and tigge is saved.
+// if theHero enters coordinates .. w/o key he will see the "chest" and hear the cries but will be prompted to find the key.
+
+// how to pick up the key.. enter room with key in it. see sth shiny. Pick up the key? y/n
+
+// add other items to pick up.. fiends to defeat
+// solve randomized message when hitting walls. 
